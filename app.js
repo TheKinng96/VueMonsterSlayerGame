2b48d06ac67a065a8589165f7d4c81e0b622b901
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-07-17 16:26:30 +0900

    finished game logic and mobile responsiveness

--- app.js
+++ app.js
@@ -3,33 +3,55 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameStarted: false
+    gameStarted: false,
+    turns:[]
   },
   methods: {
     startGame: function () {
       this.gameStarted = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function () {
-      this.monsterHealth -= this.damageCal(10, 3);
-
+      var damage = this.damageCal(10, 3);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'You damage the monster for ' + damage
+      })
       if (this.gameCheck()) {
         return;
       }
 
-      this.playerHealth -= this.damageCal(12, 5);
-
-      this.gameCheck();
+      this.monsterAttack();
     },
     specialAttack: function () {
-      
+      var damage = this.damageCal(20, 0)
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Magic gave the monster ' + damage + ' hit'
+      })
+      if (this.gameCheck()) {
+        return;
+      }
+      this.monsterAttack();
     },
     heal: function () {
-      
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'You healed 10'
+      })
+      this.monsterAttack();
     },
     run: function () {
-      
+      this.gameStarted = false;
     },
     damageCal: function (max, min) {
       return damage = Math.max(Math.floor(Math.random() * max), min);
@@ -51,6 +73,15 @@ new Vue({
         return true;
       }
       return false;
+    },
+    monsterAttack: function () {
+      var damage = this.damageCal(12, 5);
+      this.playerHealth -= damage;
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hit you for ' + damage
+      })
+      this.gameCheck();
     }
   }
 })
