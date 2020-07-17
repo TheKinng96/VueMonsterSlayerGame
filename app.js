4640e2c844d1e506897a7c3c8d1e3ff31e2500c9
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-07-17 15:15:11 +0900

    attack function on the board!!!

--- app.js
+++ app.js
@@ -10,6 +10,47 @@ new Vue({
       this.gameStarted = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+    },
+    attack: function () {
+      this.monsterHealth -= this.damageCal(10, 3);
+
+      if (this.gameCheck()) {
+        return;
+      }
+
+      this.playerHealth -= this.damageCal(12, 5);
+
+      this.gameCheck();
+    },
+    specialAttack: function () {
+      
+    },
+    heal: function () {
+      
+    },
+    run: function () {
+      
+    },
+    damageCal: function (max, min) {
+      return damage = Math.max(Math.floor(Math.random() * max), min);
+    },
+    gameCheck: function () {
+      if (this.monsterHealth <= 0) {
+        if (confirm('Monster defeated! Have another battle?')) {
+          this.startGame()
+        } else {
+          this.gameStarted = false
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm('You have been killed... Another life?')) {
+          this.startGame()
+        } else {
+          this.gameStarted = false
+        }
+        return true;
+      }
+      return false;
     }
   }
 })
